5b6c52d1fa4f40302ac96df84850a886be64ee18
Tashi Dolma <tadolma97@gmail.com>
2022-08-12 11:24:14 -0400

    commit tree 879eca52a4041f839428cded7afb274b0b2dcd13

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,7 @@
 require_relative './node'
 
 class LinkedList
-  attr_accessor :head, :tail
+  attr_accessor :head, :tail, :length
 
   def initialize
     @head = nil
@@ -9,6 +9,26 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
-  end
+		if (self.head == nil) 
+			print("Empty Linked List\n")
+		else
+ 
+			temp = self.head
+			back = nil
+			#  Make new tail
+			self.tail = self.head
+			#  Change node link
+			while (temp != nil) 
+				#  Make new upcoming node as to head
+				self.head = temp
+				temp = temp.next_node
+				#  Modified current node link
+				self.head.prev_node = temp
+				self.head.next_node = back
+				back = self.head
+			end
+
+		end
+
+	end
 end
